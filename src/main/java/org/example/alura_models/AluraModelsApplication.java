6c7b3bbdbf8e69e0ff748f9d7f3219f258fab8b9
Marlon Andre Vaez Aguirre <marlonvaez@mpms.mp.br>
2025-01-31 13:53:12 -0400

    Primeiro commit

---
+++ src/main/java/org/example/alura_models/AluraModelsApplication.java
@@ -0,0 +1,13 @@
+package org.example.alura_models;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraModelsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AluraModelsApplication.class, args);
+    }
+
+}
